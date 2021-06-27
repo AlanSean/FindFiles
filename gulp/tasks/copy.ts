e8f198d407ca9f383d58fda059daf8e61b2f79c7
huangqing <2316694914@qq.com>
2021-06-27 20:07:41 +0800

    build: Adjust the way you publish

--- gulp/tasks/copy.ts
+++ gulp/tasks/copy.ts
@@ -1,7 +1,7 @@
 import { task, src, dest } from 'gulp';
-
+import { dist } from '../config';
 task('copy', function () {
   return src(['package.json', 'src/tsconfig.json', 'README.md']).pipe(
-    dest('node_modules/node-find-file/')
+    dest(dist)
   );
 });
